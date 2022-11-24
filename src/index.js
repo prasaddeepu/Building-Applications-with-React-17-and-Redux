6dd9fa71e4eafff134146c6dc3a3225480dcc05c
prasaddeepu <82377218+prasaddeepu@users.noreply.github.com>
2022-11-24 09:58:47 +0000

    modified index.js & linting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function Hi() {
-    return <p>Hi.</p>
+    return <p>Hi Prasad.</p>
 }
 
 render(<Hi />, document.getElementById("app"));
